093011a81c615c1298e709de8a9068c19aec42d9
Taxato <131350828+Taxato@users.noreply.github.com>
2023-05-15 11:56:54 +0200

    Add files via upload

---
+++ Mimir-koden/main.gd
@@ -0,0 +1,16 @@
+extends Node
+
+@onready var board = $Board
+@onready var Player = $Player
+
+var players = []
+var num_selected_players = 6 # Will be set by start menu
+
+
+func _ready():
+	pass
+#	for player in num_selected_players:
+#		players.push_back(Player.instantiate())
+#	print(players)
+
+
