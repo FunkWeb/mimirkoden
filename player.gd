76c03ea573b744dacea84895eb8a707aae7fe070
Tormod-Rasmussen <tormodrasmussen@gmail.com>
2023-05-16 11:44:08 +0200

    added move sfx

--- player.gd
+++ player.gd
@@ -10,10 +10,12 @@ var battery
 var new_tile # new tile just moved to
 var used_tiles # track tiles moved to in current turn
 var walk_walls # if card used to walk through walls
+var move_sound
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	board = $"../Board"
+	move_sound = $"../AudioStreamPlayer"
 	# edited values for testing:
 	battery = 10
 	moves = 4
@@ -63,7 +65,6 @@ func move_player(clicked_cell):
 
 func _on_board_clicked():
 	if !active_player or moves == 0:
-		end_turn(used_tiles)
 		return
 	
 	var neighbors = board.get_valid_neighbors(current_cell)
@@ -72,13 +73,18 @@ func _on_board_clicked():
 	if clicked_cell not in neighbors:
 		return
 	move_player(clicked_cell)
+	move_sound.play()
 	new_tile_effect(new_tile)
+	if moves == 0:
+		# Maybe have an end turn button so the player can use cards after their last move
+		end_turn(used_tiles)
 
 func end_turn(used_tile_list):
 	used_tile_list.pop_back() # remove last tile so it stays occupied for next player
 	for tile in used_tile_list:
 		var index = board.get_index_from_coor(tile)
 		board.tile_list[index].occupied = false
+
 	used_tiles.clear()
 	# switch player, make sure current tile is added to used_tiles at start of turn
 	used_tiles = [current_cell]
