01946a2b3e251600c245f500c25d91182efc6c26
bo <bo.bratsberg@funkweb.org>
2023-05-22 13:25:09 +0200

    merged ui into main

--- main.gd
+++ main.gd
@@ -1,10 +1,34 @@
 extends Node
 
-var board
-var player1
+@onready var board = $Board
+@onready var Player = $Player
+
+var players = []
+var num_selected_players = 6 # Will be set by start menu
+
 
 func _ready():
-	board = $Board
-	player1 = $Player
+#	pass
+	$QuitUI.hide()
+	$StartUI.update_player_count(num_selected_players)
+	
+	
+
+func _process(delta):
+	if Input.is_action_pressed("ui_cancel"):
+		$QuitUI.show()
+		$StartUI.hide()
+
+func _on_start_ui_start_game():
+	for player in num_selected_players:
+		players.push_back(Player.duplicate())
+	
+	for player in players:
+		player.set_position(Vector2(200,200))
+		print(player.position)
+	$StartUI.hide()
 
 
+func _on_start_ui_players(num: int):
+	num_selected_players = num
+	
