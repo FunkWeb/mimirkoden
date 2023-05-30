ad90e50f5c60a8a74f449bb3cbd375d54391ea4a
bo <bo.bratsberg@funkweb.org>
2023-05-30 11:52:52 +0200

    working on player ui

--- main.gd
+++ main.gd
@@ -6,6 +6,9 @@ extends Node
 var players = []
 var num_selected_players = 6 # Will be set by start menu
 
+var player_uis = []
+var player_ui_positions = [Vector2(20,20),Vector2(20,580),Vector2(20,280),
+	Vector2(1000,20),Vector2(1000,580),Vector2(1000,280)]
 
 func _ready():
 #	pass
@@ -25,10 +28,28 @@ func _on_start_ui_start_game():
 	
 	for player in players:
 		player.set_position(Vector2(200,200))
+		player.show()
 		print(player.position)
+	
+	set_player_ui()
+	
+	Player.hide()
+	
 	$StartUI.hide()
 
 
 func _on_start_ui_players(num: int):
 	num_selected_players = num
 	
+
+func set_player_ui():
+	var index = 0
+	for player in players:
+		var ui = $PlayerUI.duplicate()
+		ui.pos = player_ui_positions[index]
+		ui.player = player
+		ui.show()
+		player_uis.push_back(ui)
+		index += 1
+		
+	$PlayerUI.hide()
--- playerui.gd
+++ playerui.gd
@@ -2,11 +2,18 @@ extends CanvasLayer
 
 @onready var player = $".."/Player
 
+var pos = Vector2(0,0)
+var element_offset = Vector2(0,20)
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	_on_player_update_ui()
-	pass # Replace with function body.
+	
+func place():
+	$Name.position = pos
+	$ChargeCounter.position = pos + element_offset
+	$KeyCounter.position = pos + (element_offset * 2)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -17,4 +24,7 @@ func _process(delta):
 func _on_player_update_ui():
 	$KeyCounter.text = "Keys: "+str(player.keys)
 	$ChargeCounter.text = "Charges: "+str(player.battery)
-	pass # Replace with function body.
+	print("players "+str(len($"..".players)))
+	for player in $"..".players:
+		print("pos "+str(player.position))
+	print("basicpos "+str($"../Player".position))
