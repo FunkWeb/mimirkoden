e8924411f41e4700fd1ba9c20b0ae81f52109de1
Tormod-Rasmussen <tormodrasmussen@gmail.com>
2023-06-19 14:02:24 +0200

    start_ui buttons toggle

--- start_ui_.gd
+++ start_ui_.gd
@@ -2,11 +2,15 @@ extends Control
 
 var num_players = 2
 
-@onready var main = $".."
-@onready var select_sound = $"../SelectSound"
-
 func _ready():
 	update_player_count()
+
+func unpress_other_buttons(button_num):
+	for i in range(2,7):
+		if i == button_num:
+			continue
+		var button_node = get_node( "Button" + str(i))
+		button_node.button_pressed = false
 
 func update_player_count():
 	$Players.text = "Spillere: {num}".format({"num":num_players})
@@ -18,18 +22,28 @@ func player_count_button_pressed(id):
 
 func _on_button_2_pressed():
 	player_count_button_pressed(2)
+	$Button2.button_pressed = true
+	unpress_other_buttons(2)
 
 func _on_button_3_pressed():
 	player_count_button_pressed(3)
+	$Button3.button_pressed = true
+	unpress_other_buttons(3)
 
 func _on_button_4_pressed():
 	player_count_button_pressed(4)
+	$Button4.button_pressed = true
+	unpress_other_buttons(4)
 
 func _on_button_5_pressed():
 	player_count_button_pressed(5)
+	$Button5.button_pressed = true
+	unpress_other_buttons(5)
 
 func _on_button_6_pressed():
 	player_count_button_pressed(6)
+	$Button6.button_pressed = true
+	unpress_other_buttons(6)
 
 func _on_start_pressed():
 	GameManager.play_select_sound()
