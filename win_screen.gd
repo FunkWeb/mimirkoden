12f2afa62a3fe8d443769b888c48c710f0cc4f6e
Tormod-Rasmussen <tormodrasmussen@gmail.com>
2023-06-19 12:25:15 +0200

    win_screen text og return to menu

--- win_screen.gd
+++ win_screen.gd
@@ -1,6 +1,12 @@
 extends Node
 
+func _ready():
+	$win_label.text = "{name} vant!".format({"name":GameManager.winning_player})
+	GameManager.play_win_music()
 
-func _on_button_quit_button_down():
-	get_tree().quit()
+func _on_button_return_pressed():
+	GameManager.play_background_music()
+	GameManager.change_scene_to_menu()
 
+func _on_button_quit_pressed():
+	GameManager.quit_game()
