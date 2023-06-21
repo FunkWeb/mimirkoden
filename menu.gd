caa51766c18ed1aff780ec97d17ae955763ab395
Tormod-Rasmussen <tormodrasmussen@gmail.com>
2023-06-21 10:34:02 +0200

    meny knapper select_sound

--- menu.gd
+++ menu.gd
@@ -1,9 +1,11 @@
 extends Node
 
 func _on_button_rules_pressed():
+	GameManager.play_select_sound()
 	GameManager.change_scene_to_rules()
 
 func _on_button_credits_pressed():
+	GameManager.play_select_sound()
 	GameManager.change_scene_to_credits()
 
 func _on_button_quit_pressed():
